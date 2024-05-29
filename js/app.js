64739b9a39819f0100afb6fc68dde136436e02c3
Dize dev <axiozweb2022@gmail.com>
2024-05-29 11:41:10 -0500

    se creo la funcion de agregar la pregunta i las opciones en la patalla

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+import { questions } from "./question.js";
 
 const startBtn = document.querySelector('.start_btn button'),
     infoBox = document.querySelector('.info-box'),
@@ -13,4 +14,40 @@ continueBtn.addEventListener('click', () => {
 
     infoBox.classList.remove('active');
     quizBox.classList.add('activeQuiz');
+    showQuestion(0);
 });
+
+let queCount = 0;
+
+const showQuestion = index => {
+
+    const quuText = document.querySelector('.que_text');
+    const optionList = document.querySelector('.option_list');
+
+    let queTag = `<p>${questions[index].question}</p>`;
+    let optionTag = `
+
+        <article class="option">
+            <p>${questions[index].options[0]}</p>
+            <span class="icon tick"><i class="ri-check-fill"></i></span>
+        </article>
+        
+        <article class="option">
+            <p>${questions[index].options[1]}</p>
+            <span class="icon croos"><i class="ri-close-line"></i></span>
+        </article>
+
+        <article class="option">
+            <p>${questions[index].options[2]}</p>
+            <span class="icon"></span>
+        </article>
+
+        <article class="option">
+            <p>${questions[index].options[3]}</p>
+            <span class="icon"></span>
+        </article>
+    `;
+
+    quuText.innerHTML = queTag;
+    optionList.innerHTML = optionTag
+};
---
+++ js/question.js
@@ -0,0 +1,59 @@
+
+export const questions = [
+    {
+        numb: 1,
+        question: '¿Qué significa HTML?',
+        answer: 'Hyper text preprocessor',
+        options: [
+            'Hyper text preprocessor',
+            'Hyper text markup language',
+            'Hyper text multiple language',
+            'Hyper tools multi language'
+        ]
+    },
+    {
+        numb: 2,
+        question: '¿Cuál es el lenguaje de marcado utilizado para crear páginas web?',
+        answer: 'HTML',
+        options: [
+            'CSS',
+            'JavaScript',
+            'HTML',
+            'Python'
+        ]
+    },
+    {
+        numb: 3,
+        question: '¿Qué significa CSS?',
+        answer: 'Cascading Style Sheets',
+        options: [
+            'Cascading Style Sheets',
+            'Computer Style Sheets',
+            'Creative Style Sheets',
+            'Colorful Style Sheets'
+        ]
+    },
+    {
+        numb: 4,
+        question: '¿Cuál es el lenguaje de programación utilizado para agregar interactividad a las páginas web?',
+        answer: 'JavaScript',
+        options: [
+            'Java',
+            'Python',
+            'JavaScript',
+            'Ruby'
+        ]
+    },
+    {
+        numb: 5,
+        question: '¿what does XLM stand for?',
+        answer: 'extansible markup Languaje',
+        options: [
+            'extansible markup Languaje',
+            'extansible multiple Languaje',
+            'extansible muilti-program Languaje',
+            'examine multiple Languaje'
+        ]
+    },
+];
+
